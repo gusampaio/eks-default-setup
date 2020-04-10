ea335edf555f53dca6927c84df768346dd8d172f
Sampaio <sampaig@88e9fe4f024f.ant.amazon.com>
2020-04-10 16:25:48 +0100

    added script for Launching CA

---
+++ README.md
@@ -0,0 +1,11 @@
+# Required Add ons to install over cluster
+
+- [Cluster Auto Scaler](https://docs.aws.amazon.com/eks/latest/userguide/cluster-autoscaler.html)
+- Kubernetes Dashboard
+- Horizontal Pod AutoScaler
+- Jenkins Server
+- ALB Ingress
+- Grafana
+- Prometheus
+- Example App Running
+- Troubleshooting Pod
--- initialize-cluster.py
+++ initialize-cluster.py
@@ -1,34 +1,78 @@
-import sys, argparse
+import sys, argparse, os
+import urllib.request
+import subprocess as sp
+
+arn=sp.getoutput("kubectl config current-context")
+print(arn)
+clusterName=arn.split("/")[-1]
 
 def main():
     dispatcher = { 'all': createAll , 
                    'cluster_autoscaler': createCA ,
                    'horizontal_pod_autoscaler': createHPA
-                 }
+                }
     
     # receive the arguments passed at the initailization of the command
     functions = buildArguments()
     
     for f in functions:
         dispatcher[f]()
-        
+
 
 def createAll():
     print("Installing everything")
 
-def createCA():
-    print("Installing CA")
+# Create Cluster AutoScaler Folders and apply commands 
+def createCA():    
+    print("Installing CA...")
+    
+    name="CA"
+    gistURL="https://raw.githubusercontent.com/kubernetes/autoscaler/master/cluster-autoscaler/cloudprovider/aws/examples/cluster-autoscaler-autodiscover.yaml"
+    dirFileName=name + "/cluster-autoscaler-autodiscover.yaml"
+    
+    # Creating local directory
+    createLocalDir(name)
+    # Downloading template
+    downlodaTemplate(gistURL, dirFileName)
+    # Updating template with Correct Cluster Name
+    cmd="sed 's/<YOUR CLUSTER NAME>/" + clusterName + "/' " + dirFileName
+    print(cmd)
+    os.system(cmd)
 
+
+
+    # Applying commands
+    kubectlApply(dirFileName,name)
+
+
+# Horizontal Pod AutoScaler
 def createHPA():
-    print("Installing HPA")   
+    # Creating local
+    print("oi")
+
+
+
+def downlodaTemplate(url, fileName):
+    urllib.request.urlretrieve(url, fileName)
+
+def kubectlApply(path,name):
+    os.system("kubectl apply -f " + path)
+    print("Deployed ", name)
+
+def createLocalDir(name):
+    try:
+        os.mkdir(name)
+        print("Directory " , name , " has been created")
+    except FileExistsError:
+        print("Directory " , name ,  " already exists")
 
 def buildArguments():
     # Building args to pass over the desired ones
     parser = argparse.ArgumentParser(description='Execute Kubernetes commands to deploy Commons addons to your Cluster')
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--all", "-A", help="Install all Add ons to your Cluster", action="store_true")
-    group.add_argument("--cluster-autoscaler", "-ca", help="Install Cluster to your Cluster", action="store_true")
-    group.add_argument("--horizontal-pod-autoscaler", "-hpa", help="Install Cluster to your Cluster", action="store_true")
+    group.add_argument("-A", "--all", help="Install all Add ons to your Cluster", action="store_true")
+    group.add_argument("-ca", "--cluster-autoscaler", help="Install Cluster to your Cluster", action="store_true")
+    group.add_argument("-hpa", "--horizontal-pod-autoscaler", help="Install Cluster to your Cluster", action="store_true")
     args = parser.parse_args()
     argsTrue = []
 
